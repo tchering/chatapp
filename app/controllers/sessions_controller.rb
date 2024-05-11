63573b383911123cc8857fbdc84401c266273f89
tchering <sonam.sherpa.lama@gmail.com>
2024-05-11 17:37:10 +0200

    gender select

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,21 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      flash[:success] = "Welcome back!"
+      flash[:success] = 'Welcome back!'
       log_in @user
-      redirect_to root_path
+      redirect_to @user
     else
-      flash[:danger] = "Invalid username or password"
+      flash[:danger] = 'Invalid username or password'
       render 'new'
     end
-
   end
 
   def destroy
-   log_out if logged_in?
-   flash[:success] = "You have successfully logged out"
-   redirect_to root_path
+    log_out if logged_in?
+    flash[:success] = 'You have successfully logged out'
+    redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_124729) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_11_152307) do
   create_table "messages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "room_id", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_124729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "gender"
   end
 
   add_foreign_key "messages", "rooms"
